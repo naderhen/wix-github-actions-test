292696337abf3de3178a9570518f8e9b2ceaca4e
Nader Hendawi <naderhen@gmail.com>
2021-08-04 16:11:44 -0400

    m
    Signed-off-by: Nader Hendawi <naderhen@gmail.com>

--- wix/make_installer.bat
+++ wix/make_installer.bat
@@ -3,6 +3,5 @@ del installer.msi
 del installer.wixobj
 del installer.wixpdb
 
-@REM copy "..\dist\Mark43 First Responder Setup 1.0.7.exe" installer.exe
 candle installer.wxs -ext WixUtilExtension -dProductVersion=1.0.7
 light installer.wixobj -ext WixUtilExtension -dProductVersion=1.0.7
